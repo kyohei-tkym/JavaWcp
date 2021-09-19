1e1c9ba327400d65b33f6e49b8cead5d383a8003
takayamakyohei <kyohe1208@gmail.com>
2021-09-19 10:28:44 +0900

    [finish]chapter4

---
+++ src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] agrgs) {
+		String name = "Kyohei Takayama";
+		System.out.println(name);
+		
+		name = "fun";
+		System.out.println(name);
+		
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
