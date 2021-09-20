c5fb4883a666c02c0811b91407bb3c4ebab663fc
takayamakyohei <kyohe1208@gmail.com>
2021-09-20 22:20:42 +0900

    [finsh]chapter5

---
+++ src/Chapter05.java
@@ -0,0 +1,30 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 1234567890000L;
+		System.out.println(long1);
+		
+		double double1 = 1413.34;
+		System.out.println(double1);
+		
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+	
+		Integer int1 = 1000;
+		System.out.println(int1);
+		
+		Character char1 = 'あ';
+		System.out.println(char1);
+		
+		Float flt1 = 1.1f;
+		System.out.println(flt1);
+		
+		int[] array1 = new int[5];
+		array1[0] = 10;
+		array1[1] = 37;
+		array1[2] = 7;
+		array1[3] = 22;
+		array1[4] = 25;
+		System.out.println(array1[3]);
+	}
+}
